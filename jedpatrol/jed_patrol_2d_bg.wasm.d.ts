44352dcf05493589f197895e49d8241d847381b1
Eduardo Jedliczka <edujed@gmail.com>
2026-09-14 20:37:16 -0300

    portifolio: jedpatrol

---
+++ jedpatrol/jed_patrol_2d_bg.wasm.d.ts
@@ -0,0 +1,22 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const main: (a: number, b: number) => number;
+export const __wasm_bindgen_func_elem_59262: (a: number, b: number, c: number, d: number) => void;
+export const __wasm_bindgen_func_elem_59323: (a: number, b: number, c: number, d: number) => void;
+export const __wasm_bindgen_func_elem_71958: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_71958_3: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_71958_4: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_71958_5: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_71958_6: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_71958_7: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_71958_8: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_71958_9: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_72053: (a: number, b: number) => void;
+export const __wbindgen_export: (a: number, b: number) => number;
+export const __wbindgen_export2: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_export3: (a: number) => void;
+export const __wbindgen_export4: (a: number, b: number, c: number) => void;
+export const __wbindgen_export5: (a: number, b: number) => void;
+export const __wbindgen_add_to_stack_pointer: (a: number) => number;
+export const __wbindgen_start: () => void;
